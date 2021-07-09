c177452eb1dfeb120a670f62dad343fe4ed2ec12
Ayaro <Ayaro1@users.noreply.github.com>
2021-07-09 04:29:36 +0200

    Update localization.en.lua
    Update

--- DBM-Icecrown/localization.en.lua
+++ DBM-Icecrown/localization.en.lua
@@ -148,7 +148,8 @@ L:SetOptionLocalization{
 	SetIconOnDominateMind		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(71289),
 	SetIconOnDeformedFanatic	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(70900),
 	SetIconOnEmpoweredAdherent	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(70901),
-	SoundWarnCountingMC			= "Play a 5 second audio countdown for Mind Control"
+	SoundWarnCountingMC			= "Play a 5 second audio countdown for Mind Control",
+	RemoveDruidBuff				= "Remove Druid buff 24 seconds into the fight"
 }
 
 L:SetMiscLocalization{
