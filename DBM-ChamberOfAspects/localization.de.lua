b63b26b975301d3c275f514f60bb40169703be31
Ayaro <Ayaro1@users.noreply.github.com>
2021-10-29 20:11:31 +0200

    Update localization.de.lua

--- DBM-ChamberOfAspects/localization.de.lua
+++ DBM-ChamberOfAspects/localization.de.lua
@@ -145,7 +145,7 @@ L:SetTimerLocalization({
 
 L:SetOptionLocalization({
 	WarnAdds		= "Verkünde neue Adds",
-	TimerAdds		= "Zeige Timer für neue Adds"
+	TimerAdds		= "Zeige Timer für neue Adds",
 	AddsArrive		= "Show timer for adds arrival", --Needs Translating
 	warnCleaveArmor	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(74367)
 })
